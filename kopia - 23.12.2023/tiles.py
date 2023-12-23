6d300a07f1b78311a71fd491a03d47b76485661e
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2023-12-23 20:07:37 +0100

    Upload New File

---
+++ kopia - 23.12.2023/tiles.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import SQUARE_SIZE, WHITE, BLACK
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, letter, position):
+        super().__init__()
+
+        self._letter = letter
+        self._position = position
+
+        self.image = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
+        self.image.fill(WHITE)
+        pygame.draw.rect(self.image, BLACK, self.image.get_rect(), 1)
+
+        font = pygame.font.Font(None, 50)
+        text = font.render(str(letter), True, BLACK)
+        text_rect = text.get_rect(center=self.image.get_rect().center)
+        self.image.blit(text, text_rect)
+
+        self.rect = self.image.get_rect(topleft=position)
+
+    def letter(self):
+        return self._letter
+
+    def position(self):
+        return self._position
