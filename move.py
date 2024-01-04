ee5ff4da63f207aa13c4aca4d9eafdea285e794d
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2024-01-04 09:49:12 +0100

    made move class

---
+++ move.py
@@ -0,0 +1,28 @@
+class Move:
+    # najpierw patrzymy na położenie, a potem na valid_words
+    def __init__(self):
+        self.click = []
+        self.current_word = {}
+
+    def click_handling(self, player, board, board_sprite):
+        """
+        Function that handles invalid mouse clicks
+        """
+        count = len(self.click)
+        if (
+            (count == 1 and ((self.click[0][0] != 16)))
+            or (count == 1 and (self.click[0][1] > 10))
+            or (count == 1 and (self.click[0][1] < 4))
+            or (count == 1 and (player.rack()[self.click[0][1] - 4] == ""))
+            or (count == 2 and (self.click[1][0] > 14))
+            or (
+                count == 2
+                and (
+                    board.colid(
+                        board_sprite,
+                        self.click,
+                    )
+                )
+            )
+        ):
+            self.click.clear()
