e32a7de3a3d12666441fa1deb35a293370c53448
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2024-01-04 13:09:21 +0100

    removed blank handler

--- bot.py
+++ bot.py
@@ -18,33 +18,6 @@ class Bot(Player):
     def __init__(self, name="Bot", words=None, rack=None):
         super().__init__(name, words, rack)
 
-    def blank_handler(self, word, letter_list, choice):
-        blank_count = self.rack().count(" ")
-        not_in_rack = {}
-        choice = [choice]
-
-        not_in_rack = {
-            letter: word.count(letter)
-            - self.rack().count(letter)
-            - choice.count(letter)
-            for letter in set(word)
-            if letter not in not_in_rack
-            and word.count(letter) > self.rack().count(letter)
-        }
-        all_count = sum(not_in_rack.values())
-
-        if all_count <= blank_count:
-            for e, letter in enumerate(word):
-                if letter in not_in_rack and word.count(letter) == not_in_rack[letter]:
-                    letter_list[e] = " "
-                elif letter in not_in_rack and not_in_rack[letter] != 0:
-                    letter_list[e] = " "
-                    not_in_rack[letter] -= 1
-                else:
-                    letter_list[e] = letter
-            return letter_list
-        return []
-
     def valid_first_word(self, valid_words):
         """
         Finds a valid new word for bot. Manages blank tiles
@@ -72,12 +45,14 @@ class Bot(Player):
 
                 if all_count <= blank_count:
                     for e, letter in enumerate(word):
-                        if letter in not_in_rack:
-                            if word.count(letter) == not_in_rack[letter]:
-                                letter_list[e] = " "
-                            elif not_in_rack[letter] != 0:
-                                letter_list[e] = " "
-                                not_in_rack[letter] -= 1
+                        if (
+                            letter in not_in_rack
+                            and word.count(letter) == not_in_rack[letter]
+                        ):
+                            letter_list[e] = " "
+                        elif letter in not_in_rack and not_in_rack[letter] != 0:
+                            letter_list[e] = " "
+                            not_in_rack[letter] -= 1
                         else:
                             letter_list[e] = letter
                     return letter_list
@@ -199,159 +174,132 @@ class Bot(Player):
             return True
         return False
 
-    def made_word_info(
-        self,
-        board,
-        coord,
-        result,
-        choice,
-        old_word,
-        upd_word,
-        prefix,
-        sufix,
-        position,
-    ):
+    def made_word_info(self, board, result, choice, added_let, position):
+        prefix = []
+        sufix = []
         new_word = {}
-        if position == "horizontal":
-            if coord[2] - len(prefix) >= 0 and (
-                coord[2] + len(result[0]) + len(sufix) - 1 <= 14
-            ):
-                row_start = coord[1]
-                col_start_pr = coord[2] - len(prefix)
-                col_start_suf = coord[2] + len(result[0])
-                if prefix:
-                    if self.valid_add_pos(
-                        board,
-                        row_start,
-                        col_start_pr,
-                        position,
-                        prefix,
-                    ):
-                        new_word["prefix"] = [
-                            row_start,
-                            col_start_pr,
-                            position,
-                            "".join(prefix).upper(),
-                        ]
-                    else:
-                        new_word = {}
-                        return new_word
-                else:
-                    new_word["prefix"] = []
-                if sufix:
-                    if self.valid_add_pos(
-                        board,
-                        row_start,
-                        col_start_suf,
-                        position,
-                        sufix,
-                    ):
-                        new_word["sufix"] = [
-                            row_start,
-                            col_start_suf,
-                            position,
-                            "".join(sufix).upper(),
-                        ]
-                    else:
-                        new_word = {}
-                        return new_word
-                else:
-                    new_word["sufix"] = []
+
+        old_word, upd_word = result[0], result[1]
+        coord = board.exist(result[0])
+
+        for ind, original_pres in added_let:
+            if original_pres == 0:
+                prefix.append(added_let[(ind, original_pres)])
             else:
+                sufix.append(added_let[(ind, original_pres)])
+
+        if position == "horizontal" and (
+            coord[2] - len(prefix) >= 0
+            and (coord[2] + len(result[0]) + len(sufix) - 1 <= 14)
+        ):
+            row_start = coord[1]
+            col_start_pr = coord[2] - len(prefix)
+            col_start_suf = coord[2] + len(result[0])
+
+            if prefix and self.valid_add_pos(
+                board, row_start, col_start_pr, position, prefix
+            ):
+                new_word["prefix"] = [
+                    row_start,
+                    col_start_pr,
+                    position,
+                    "".join(prefix).upper(),
+                ]
+            elif prefix and not self.valid_add_pos(
+                board, row_start, col_start_pr, position, prefix
+            ):
                 new_word = {}
                 return new_word
+            elif not prefix:
+                new_word["prefix"] = []
+
+            if sufix and self.valid_add_pos(
+                board, row_start, col_start_suf, position, sufix
+            ):
+                new_word["sufix"] = [
+                    row_start,
+                    col_start_suf,
+                    position,
+                    "".join(sufix).upper(),
+                ]
+            elif sufix and not self.valid_add_pos(
+                board, row_start, col_start_suf, position, sufix
+            ):
+                new_word = {}
+                return new_word
+            elif not sufix:
+                new_word["sufix"] = []
+
+        elif position == "vertical" and (
+            coord[1] - len(prefix) >= 0
+            and coord[1] + len(result[0]) + len(sufix) - 1 <= 14
+        ):
+            row_start_pr = coord[1] - len(prefix)
+            row_start_suf = coord[1] + len(result[0])
+            col_start = coord[2]
+            if prefix and self.valid_add_pos(
+                board, row_start_pr, col_start, position, prefix
+            ):
+                new_word["prefix"] = [
+                    row_start_pr,
+                    col_start,
+                    position,
+                    "".join(prefix).upper(),
+                ]
+            elif prefix and not self.valid_add_pos(
+                board, row_start_pr, col_start, position, prefix
+            ):
+                new_word = {}
+                return new_word
+            elif not prefix:
+                new_word["prefix"] = []
+
+            if sufix and self.valid_add_pos(
+                board, row_start_suf, col_start, position, sufix
+            ):
+                new_word["sufix"] = [
+                    row_start_suf,
+                    col_start,
+                    position,
+                    "".join(sufix).upper(),
+                ]
+            elif sufix and not self.valid_add_pos(
+                board, row_start_suf, col_start, position, sufix
+            ):
+                new_word = {}
+                return new_word
+            elif not sufix:
+                new_word["sufix"] = []
+
         else:
-            if (
-                coord[1] - len(prefix) >= 0
-                and coord[1] + len(result[0]) + len(sufix) - 1 <= 14
-            ):
-                row_start_pr = coord[1] - len(prefix)
-                row_start_suf = coord[1] + len(result[0])
-                col_start = coord[2]
-                if prefix:
-                    if self.valid_add_pos(
-                        board,
-                        row_start_pr,
-                        col_start,
-                        position,
-                        prefix,
-                    ):
-                        new_word["prefix"] = [
-                            row_start_pr,
-                            col_start,
-                            position,
-                            "".join(prefix).upper(),
-                        ]
-                    else:
-                        new_word = {}
-                        return new_word
-                else:
-                    new_word["prefix"] = []
-                if sufix:
-                    if self.valid_add_pos(
-                        board,
-                        row_start_suf,
-                        col_start,
-                        position,
-                        sufix,
-                    ):
-                        new_word["sufix"] = [
-                            row_start_suf,
-                            col_start,
-                            position,
-                            "".join(sufix).upper(),
-                        ]
-                    else:
-                        new_word = {}
-                        return new_word
-                else:
-                    new_word["sufix"] = []
-            else:
-                new_word = {}
-                return new_word
+            new_word = {}
+            return new_word
+
         new_word["old_word"] = old_word.upper()
         new_word["new_word"] = upd_word.upper()
         new_word["mode"] = choice
         return new_word
 
-    def added_info(self, board, valid_words, prefix, sufix, choice, new_word):
+    def added_info(self, board, valid_words, choice):
+        new_word = {}
+
         result = (
             self.add_to(valid_words, board.word_list)
             if choice == "added"
             else self.new_word(board, valid_words)[0]
         )
+
         old_word, upd_word = result[0], result[1]
-        if old_word != "" and upd_word != "":
-            added_let = self.added_letters(result[0], result[1])
-            if added_let:
-                for ind, original_pres in added_let:
-                    if original_pres == 0:
-                        prefix.append(added_let[(ind, original_pres)])
-                    else:
-                        sufix.append(added_let[(ind, original_pres)])
-                if board.exist(result[0]) is not False:
-                    coord = board.exist(result[0])
-                    if len(old_word) == 1:
-                        old_info = board.exist(self.new_word(board, valid_words)[1])
-                        position = (
-                            "horizontal" if old_info[0] == "vertical" else "vertical"
-                        )
-                    else:
-                        position = coord[0]
-                    new_word = self.made_word_info(
-                        board,
-                        coord,
-                        result,
-                        choice,
-                        old_word,
-                        upd_word,
-                        prefix,
-                        sufix,
-                        position,
-                    )
-            else:
-                new_word = {}
-                return new_word
+        added = self.added_letters(result[0], result[1])
+        if added:
+            if board.exist(result[0]) is not False and len(old_word) == 1:
+                old_info = board.exist(self.new_word(board, valid_words)[1])
+                position = "horizontal" if old_info[0] == "vertical" else "vertical"
+            elif board.exist(result[0]) is not False and len(old_word) != 1:
+                position = board.exist(result[0])[0]
+
+            new_word = self.made_word_info(board, result, choice, added, position)
+
         return new_word
 
     def word_finding(self, board, valid_words):
@@ -359,8 +307,6 @@ class Bot(Player):
         Manages word finding for bot, including making
         a new one and adding letters to an exisitng one
         """
-        prefix = []
-        sufix = []
         new_word = {}
 
         choice = "new" if len(board.word_list) == 0 else random.choice(["new", "added"])
@@ -370,24 +316,9 @@ class Bot(Player):
             if all(info != "" for info in result):
                 new_word["info"] = result
                 new_word["mode"] = "first"
-        elif choice == "new" and len(board.word_list) != 0:
-            new_word = self.added_info(
-                board,
-                valid_words,
-                prefix,
-                sufix,
-                choice,
-                new_word,
-            )
+
         else:
-            new_word = self.added_info(
-                board,
-                valid_words,
-                prefix,
-                sufix,
-                choice,
-                new_word,
-            )
+            new_word = self.added_info(board, valid_words, choice)
         return new_word
 
     def made_current_word(self, info):
@@ -408,22 +339,12 @@ class Bot(Player):
                 cur_col += 1
         return current_word
 
-    def attempts(self, board, valid_words):
-        tries = 4
-        bot_choice = {}
-        while tries > 0:
-            bot_choice = self.word_finding(board, valid_words)
-            if not bot_choice:
-                tries -= 1
-            else:
-                return bot_choice
-        return bot_choice
-
     def valid_new(self, board, choice):
         old_word = choice["old_word"]
         new_word = choice["new_word"]
         if len(old_word) != 1:
             return True
+
         if choice["prefix"]:
             row, col, position, added = choice["prefix"]
             if (
@@ -440,8 +361,11 @@ class Bot(Player):
                 and board.board[row + len(new_word)][col] == ""
             ):
                 return True
-        elif choice["sufix"]:
+            else:
+                return False
+        if choice["sufix"]:
             row, col, position, added = choice["sufix"]
+            print(row, col, position, added)
             if (
                 position == "horizontal"
                 and self.valid_add_pos(board, row, col, position, added)
@@ -450,59 +374,72 @@ class Bot(Player):
             ):
                 return True
             elif (
-                position == "horizontal"
+                position == "vertical"
                 and self.valid_add_pos(board, row, col, position, added)
                 and board.board[row - len(old_word) - 1][col] == ""
                 and board.board[row + len(added)][col] == ""
             ):
                 return True
+            else:
+                return False
+
         return False
 
-    def made_word(self, board, board_sprite, valid_words):
+    def made_word(self, board, valid_words, bot_choice):
         """
         Updates board, word_lists
         """
         current_word = {}
-        bot_choice = self.attempts(board, valid_words)
-        if bot_choice:
-            if bot_choice["mode"] == "first":
-                info = bot_choice["info"]
-                current_word = self.made_current_word(info)
-                board.update_word_list(bot_choice["info"][3])
-                self.update_words(bot_choice["info"][3])
-            else:
-                added_prefix = bot_choice["prefix"]
-                if added_prefix:
-                    current_pref = self.made_current_word(added_prefix)
-                    current_word.update(current_pref)
-                added_sufix = bot_choice["sufix"]
-                if added_sufix:
-                    current_suf = self.made_current_word(added_sufix)
-                    current_word.update(current_suf)
-
-                if all(
-                    board.board[row][col] == "" for row, col in current_word
-                ) and self.valid_new(board, bot_choice):
-                    if bot_choice["old_word"] in board.word_list:
-                        board.word_list.remove(bot_choice["old_word"])
-
-                    board.update_word_list(bot_choice["new_word"])
-                    self.update_words(bot_choice["new_word"])
-                else:
-                    self.replace_rack(board)
-                    return {}
+        "bot_choice = self.attempts(board, valid_words)"
+        if bot_choice["mode"] == "first":
+            info = bot_choice["info"]
+            current_word = self.made_current_word(info)
+            board.update_word_list(bot_choice["info"][3])
+            self.update_words(bot_choice["info"][3])
         else:
-            self.replace_rack(board)
+            added_prefix, added_sufix = bot_choice["prefix"], bot_choice["sufix"]
+            if added_prefix:
+                current_pref = self.made_current_word(added_prefix)
+                current_word.update(current_pref)
+            if added_sufix:
+                current_suf = self.made_current_word(added_sufix)
+                current_word.update(current_suf)
+
+            if all(
+                board.board[row][col] == "" for row, col in current_word
+            ) and self.valid_new(board, bot_choice):
+                if bot_choice["old_word"] in board.word_list:
+                    board.word_list.remove(bot_choice["old_word"])
+
+                board.update_word_list(bot_choice["new_word"])
+                self.update_words(bot_choice["new_word"])
+            else:
+                self.replace_rack(board)
+                return {}
         return current_word
+
+    def attempts(self, board, valid_words):
+        tries = 10
+        bot_choice = {}
+        while tries > 0:
+            bot_choice = self.word_finding(board, valid_words)
+            if bot_choice:
+                board.current_word = self.made_word(board, valid_words, bot_choice)
+                return board.current_word
+            else:
+                tries -= 1
+        self.replace_rack(board)
+        return board.current_word
 
     def bot_turn(self, board, board_sprite, valid_words):
         """
         Manages whole bot turn
         """
-        board.current_word = self.made_word(board, board_sprite, valid_words)
+        board.current_word = self.attempts(board, valid_words)
         for pos in board.current_word:
             board.draw_tiles(board_sprite, board.current_word[pos], pos)
             letter_hand = self.rack().index(board.current_word[pos])
             self.rack()[letter_hand] = ""
         board.update_board()
+        print(board.current_word, board.word_list)
         board.current_word_empty()
